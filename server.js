d6da7fabe8363f21e650711fdf5e50a648078c8a
Weixin Zheng <zhengweixin0101@outlook.com>
2025-08-20 14:22:04 +0800

    修复跨域

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 const getArticleRoute = require('./api/getArticle');
 const getListRoute = require('./api/getList');
